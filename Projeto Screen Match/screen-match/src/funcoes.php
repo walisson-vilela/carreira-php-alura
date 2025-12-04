ca4317fe78e04fbf0dfaf742b579cc012822f585
walisson <walisson@mundowap.com.br>
2025-12-03 22:02:30 -0300

    atualizacao na estrutura do projeto

--- Projeto Screen Match/screen-match/src/funcoes.php
+++ Projeto Screen Match/screen-match/src/funcoes.php
@@ -1,8 +1,6 @@
 <?php
 
-echo "Bem-vindo(a) ao screen match!\n";
-
-function exibirAnoLancamento(int $ano): void {
+function exibeMensagemLancamento(int $ano): void {
     if ($ano > 2022) {
         echo "Esse filme é um lançamento\n";
     } elseif($ano > 2020 && $ano <= 2022) {
@@ -14,4 +12,14 @@ function exibirAnoLancamento(int $ano): void {
 
 function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
     return $planoPrime || $anoLancamento < 2020;
+}
+
+function criaFilme(string $nome, int $anoLancamento, float $nota, string $genero): array
+{
+    return [
+        'nome' => $nome,
+        'ano' => $anoLancamento,
+        'nota' => $nota,
+        'genero' => $genero
+    ];
 }
